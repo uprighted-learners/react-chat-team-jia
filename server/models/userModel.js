38859109059926944fecdc59ecf63ee01a92bc51
Ivana Carrillo <ivanae.carrillo@gmail.com>
2024-04-19 18:28:29 -0400

    added files to routes and model folders

---
+++ server/models/userModel.js
@@ -0,0 +1,23 @@
+// !THIS IS WHAT OUR SCHEMA SHOULD LOOK LIKE, BUT WE HAVE TO CHANGE IT TO CONST USERSCHEMA (CAMELCASE) ETC
+
+//const mongoose = require('mongoose');
+
+// User Schema
+// const userSchema = new mongoose.Schema({
+//   username: {
+//       type: String,
+//       required: true,
+//       unique: true
+//   },
+//   password: {
+//       type: String,
+//       required: true
+//   },
+//   timestamp: {
+//       type: Date,
+//       default: Date.now
+//   }
+// });
+
+// // Create a model for a User
+// module.exports = mongoose.model('User', userSchema);//
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,14 @@
+//THIS IS WHERE ENDPOINTS GO
+//!SHOULD LOOK LIKE THIS
+
+/*const express = require('express');
+const router = express.Router();
+const usersController = require('../controllers/usersControllers');
+
+// POST - /api/register - register a new user
+router.post('/register', usersController.registerNewUser);
+
+// POST - /api/login - login a user
+router.post('/login', usersController.loginUser);
+
+module.exports = router;*/

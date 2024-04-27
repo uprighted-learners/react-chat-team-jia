7a4396a8cf8827396b85685bccfc277cf4b93dc7
Ivana Carrillo <ivanae.carrillo@gmail.com>
2024-04-26 22:34:23 -0400

    created room CRUD functionality

--- server/models/roomModel.js
+++ server/models/roomModel.js
@@ -1,23 +1,23 @@
 // !THIS IS WHAT OUR SCHEMA SHOULD LOOK LIKE, BUT WE HAVE TO CHANGE IT TO CONST ROOMSCHEMA (CAMELCASE) ETC
 
-//const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // User Schema
-// const userSchema = new mongoose.Schema({
-//   username: {
-//       type: String,
-//       required: true,
-//       unique: true
-//   },
-//   password: {
-//       type: String,
-//       required: true
-//   },
-//   timestamp: {
-//       type: Date,
-//       default: Date.now
-//   }
-// });
+const roomSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-// // Create a model for a User
-// module.exports = mongoose.model('User', userSchema);//
+// Create a model for a User
+module.exports = mongoose.model("room", roomSchemaSchema); //

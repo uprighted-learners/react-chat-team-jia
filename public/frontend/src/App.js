ddba9dbd867e77a37b238824c3a82ca8c3eef7ad
Ivana Carrillo <ivanae.carrillo@gmail.com>
2024-05-11 22:49:25 -0400

    Added navigation page, imported browser to App.js

--- public/frontend/src/App.js
+++ public/frontend/src/App.js
@@ -1,13 +1,33 @@
-import logo from "./logo.svg";
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation.js";
 import "./App.css";
 import Auth from "./components/Auth.jsx";
 import Rooms from "./components/Rooms.js";
-function App() {
+import Messages from './components/Messages.js'
+
+export default function App() {
   return (
-    <>
-      <Rooms />
-    </>
-  );
+  
+    <Router>
+    <div>
+     <h1>App</h1>
+      <Routes>
+        <Route path="/" element={<Rooms/>}/>
+        <Route path="/" element={<Messages/>}/>
+        <Route path="/" element={<Auth/>}/>
+      </Routes>
+      </div>
+    </Router>
+  )
 }
+
+// function App() {
+//   return (
+//     <>
+//       <Rooms />
+//     </>
+//   );
+// }
 
 export default App;

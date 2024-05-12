d3bb2666660ce0206ace2e6b54578b1a9ca0ce3d
Ivana Carrillo <ivanae.carrillo@gmail.com>
2024-05-12 13:29:47 -0400

    I deleted unnecessary files

--- public/frontend/src/components/Navigation.js
+++ public/frontend/src/components/Navigation.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Navigation() {
-  return;
-  <nav>
-    Navigation
-    <ul>
-      <li>
-        <Link to="/">Rooms</Link>
-      </li>
-    </ul>
-  </nav>;
+  return (
+    <nav>
+      Navigation
+      <ul>
+        <li>
+          <Link to="/">Rooms</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }

443b1771f309e645241fb5b34c5d9c1fd0b328e8
Ivana Carrillo <ivanae.carrillo@gmail.com>
2024-05-13 13:09:01 -0400

    fixed code in Rooms.js server connects now but will not fetch rooms

--- public/frontend/src/components/Rooms.js
+++ public/frontend/src/components/Rooms.js
@@ -1,21 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Rooms() {
-  const [posts, setPosts] = useState("");
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    fetchRooms();
+  }, []);
+
   const fetchRooms = async () => {
     try {
-      const response = await fetch("localhost:8080/rooms/getAllRooms");
+      const response = await fetch("localhost:8080/rooms/getAllrooms");
       const data = await response.json();
-      {
-      }
-      setPosts(data);
-      console.log(Rooms);
+      setRooms(data);
+      // } console.log(Rooms);
     } catch (error) {
       alert("Failed to fetch rooms: " + error.message);
     }
   };
 
-  return { fetchRooms };
+  return [fetchRooms];
 
   // return (
   //   <div>
